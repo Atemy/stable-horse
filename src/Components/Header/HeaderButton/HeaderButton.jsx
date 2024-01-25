91379cfe39fd12d4a97db09feafb64b160aedc67
KarinaSokolova <lornel77@mail.ru>
2024-01-25 23:00:22 +0300

    Свертала header

---
+++ src/Components/Header/HeaderButton/HeaderButton.jsx
@@ -0,0 +1,6 @@
+import menu from '../../../images/menu.svg'
+export const HeaderButton = () => (
+    <div className='mr-[20px] justify-self-end'>
+       <img className='w-[50px] h-[50px]' src={menu}/>  
+    </div>
+);
--- src/Components/TestComponent/TestComponent.jsx
+++
@@ -1,6 +0,0 @@
-import test from '../../images/test.png'
-export const TestComponent = ({ title }) => (
-    <div className={`p-4 lg:py-10 bg-red-600`}>
-            <img src={test} alt='test'/>
-    </div>
-);
