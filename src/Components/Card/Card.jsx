efd20bd78054f50d124912d376ca386c1b4f18b8
KarinaSokolova <lornel77@mail.ru>
2024-01-29 11:13:35 +0300

    Отцентровала кнопки

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -6,16 +6,16 @@ export const Card = ({ HorseName }) => (
         {HorseName ? (
             <>
                 <img className='mx-auto mt-[10px] mb-[2px]' src={horse_candle} alt='active_horse' />
-                <p className='font-display text-[24px] leading-[29px] mb-[20px]'>{HorseName}</p>
-                <Button className='mb-[20px]'  title={'Кормление'}/>
+                <p className='text-center font-display text-[24px] leading-[29px] mb-[20px]'>{HorseName}</p>
+                <Button className='block mx-auto mb-[20px]'  title={'Кормление'}/>
                 {/* to do ждем исправления макетов*/}
-                <Button title={'Подробнее'}/>
+                <Button className='block mx-auto' title={'Подробнее'}/>
             </>
         ) : (
             <>
                 <img className='mx-auto mt-[47px] mb-[9px]' src={rocking_horse} alt='inactive_horse' />
-                <p className='font-display mx-auto  text-[24px] leading-[29px] mb-[21px]'>Cвободно</p>
-                <Button className='h-[50px] mx-auto '  title={'Добавить'}/>
+                <p className='text-center font-display  text-[24px] leading-[29px] mb-[21px]'>Cвободно</p>
+                <Button className='block mx-auto h-[50px] '  title={'Добавить'}/>
             </>
         )
         }
